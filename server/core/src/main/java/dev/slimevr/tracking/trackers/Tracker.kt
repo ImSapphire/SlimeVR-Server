f531d7a05e092cb75f3a48524bb10720be736cf7
Butterscotch! <bscotchvanilla@gmail.com>
2025-10-28 04:48:00 -0400

    Print Y offsets for tracker and HMD

--- server/core/src/main/java/dev/slimevr/tracking/trackers/Tracker.kt
+++ server/core/src/main/java/dev/slimevr/tracking/trackers/Tracker.kt
@@ -511,7 +511,7 @@ class Tracker @JvmOverloads constructor(
 							}
 						}
 
-						LogManager.info("[Accel] Tracker $id (${trackerPosition?.designation}):\nTracker: $tracker\nHmd: $hmd\nErr: ${tracker.len() - hmd.len()}\nResult: $mountVec ($mountText)")
+						LogManager.info("[Accel] Tracker $id (${trackerPosition?.designation}):\nTracker: $trackerOff\nHmd: $hmdOff\nErr: ${tracker.len() - hmd.len()}\nResult: $mountVec ($mountText)")
 
 						if (resetNext) {
 							resetNext = false
