1a7e9f685d783806e6656a5b43ce56789a5b71c0
Butterscotch! <bscotchvanilla@gmail.com>
2025-11-05 07:11:54 -0500

    Implement optimizable accel unit test

--- server/core/src/test/java/dev/slimevr/unit/AccelCalTests.kt
+++ server/core/src/test/java/dev/slimevr/unit/AccelCalTests.kt
@@ -2,15 +2,18 @@ package dev.slimevr.unit
 
 import com.jme3.math.FastMath
 import dev.slimevr.unit.TrackerTestUtils.assertQuatApproxEqual
-import dev.slimevr.unit.TrackerTestUtils.assertVectorApproxEqual
+import io.github.axisangles.ktmath.EulerAngles
+import io.github.axisangles.ktmath.EulerOrder
 import io.github.axisangles.ktmath.Quaternion
 import io.github.axisangles.ktmath.Vector3
 import org.junit.jupiter.api.Test
-import kotlin.math.abs
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
 class AccelCalTests {
+	data class Tracker(val boneLength: Float, val trackerPlacement: Float, val attachmentFix: Quaternion, val yawFix: Quaternion)
+	data class TrackerFrame(val headAccel: Vector3, val rot: Quaternion, val accel: Vector3, val timeOffset: Float)
+
 	fun centripetalAccel(rot1: Quaternion, rot2: Quaternion, length: Float, time: Float): Vector3 {
 		// We only care about velocity magnitude for this, accel is always to the head
 		val angleDiff = rot1.angleToR(rot2)
@@ -29,23 +32,26 @@ class AccelCalTests {
 		return -((rot2 * rot1).sandwich(Vector3.NEG_Y) * centripetalAccelMag)
 	}
 
-	fun accelError(headAccel: Vector3, trackerAccel: Vector3, rot1: Quaternion, rot2: Quaternion, length: Float, time: Float, attachment: Quaternion, yaw: Quaternion): Float {
-		// Local accel
-		val adjRot1 = yaw * rot1 * attachment
-		val adjRot2 = yaw * rot2 * attachment
-		val centripetalAccel = centripetalAccel(adjRot1, adjRot2, length, time)
+	fun centripetalAccel(tracker: Tracker, lastFrame: TrackerFrame, curFrame: TrackerFrame): Vector3 = centripetalAccel(lastFrame.rot, curFrame.rot, tracker.boneLength, curFrame.timeOffset)
 
-		// Global accel
-		val adjAccel = (adjRot2 * attachment.inv()).sandwich(trackerAccel)
-		val actualCentripetal = adjAccel - headAccel
+	fun accelError(tracker: Tracker, oldFrame: TrackerFrame?, newFrame: TrackerFrame): Float {
+		if (oldFrame == null) return 0f
+
+		val calOldFrame = rawToCalib(tracker, null, oldFrame)
+		val calNewFrame = rawToCalib(tracker, null, newFrame)
+
+		val estimatedCentripetal = centripetalAccel(tracker, calOldFrame, calNewFrame)
+		val actualCentripetal = calNewFrame.accel - calNewFrame.headAccel
 
 		// Working without trackerPlacement, so magnitude of local accel is unknown
 		// Primarily consider centripetal accel if it has reasonable magnitude
-		return if (centripetalAccel.len() > 0.01f) {
-			centripetalAccel.angleTo(actualCentripetal)
+		return if (estimatedCentripetal.len() > 0.01f) {
+			estimatedCentripetal.angleTo(actualCentripetal)
+			// Or to estimate the placement too
+			// ((estimatedCentripetal * tracker.trackerPlacement) - actualCentripetal).len()
 		} else {
 			// Otherwise, consider global accel if it has reasonable magnitude
-			if (headAccel.len() > 0.01f) {
+			if (calNewFrame.headAccel.len() > 0.01f) {
 				actualCentripetal.len()
 			} else {
 				// Otherwise just return -1 for invalid
@@ -54,52 +60,120 @@ class AccelCalTests {
 		}
 	}
 
+	fun sumAccelError(tracker: Tracker, timeline: Array<TrackerFrame>): Float = transformTimeline(tracker, timeline, ::accelError).sumOf { e -> if (e >= 0f) e.toDouble() else 0.0 }.toFloat()
+
+	inline fun <reified T> transformTimeline(tracker: Tracker, timeline: Array<TrackerFrame>, operation: (tracker: Tracker, oldFrame: TrackerFrame?, newFrame: TrackerFrame) -> T): Array<T> {
+		if (timeline.isEmpty()) {
+			return emptyArray<T>()
+		}
+
+		val newTimeline = arrayOfNulls<T>(timeline.size)
+		for (i in timeline.indices) {
+			val lastFrame = if (i > 0) timeline[i - 1] else null
+			val frame = timeline[i]
+
+			newTimeline[i] = operation(tracker, lastFrame, frame)
+		}
+
+		@Suppress("UNCHECKED_CAST")
+		return newTimeline as Array<T>
+	}
+
+	fun estimateAccel(tracker: Tracker, oldFrame: TrackerFrame?, newFrame: TrackerFrame): TrackerFrame = if (oldFrame == null) {
+		newFrame.copy(
+			accel = newFrame.headAccel,
+		)
+	} else {
+		val centripetalAccel = centripetalAccel(tracker, oldFrame, newFrame)
+		newFrame.copy(
+			accel = newFrame.headAccel + (centripetalAccel * tracker.trackerPlacement),
+		)
+	}
+
+	fun calibToRaw(tracker: Tracker, oldFrame: TrackerFrame?, newFrame: TrackerFrame): TrackerFrame {
+		val rawRot = tracker.yawFix.inv() * newFrame.rot * tracker.attachmentFix.inv()
+		val rawAccel = (newFrame.rot * tracker.attachmentFix.inv()).inv().sandwich(newFrame.accel)
+
+		return newFrame.copy(
+			rot = rawRot,
+			accel = rawAccel,
+		)
+	}
+
+	fun rawToCalib(tracker: Tracker, oldFrame: TrackerFrame?, newFrame: TrackerFrame): TrackerFrame {
+		val calRot = tracker.yawFix * newFrame.rot * tracker.attachmentFix
+		val calAccel = (calRot * tracker.attachmentFix.inv()).sandwich(newFrame.accel)
+
+		return newFrame.copy(
+			rot = calRot,
+			accel = calAccel,
+		)
+	}
+
 	@Test
 	fun accelTest() {
-		// Assume frame 2 (or unspecified) is the present
-		val headAccel = Vector3(0.2f, 0f, 0f)
-		val length = 0.2f // Radius
-		val trackerPlacement = 0.15f // How far down the length the tracker is
-		val rot1 = Quaternion.IDENTITY
-		val rot2 = Quaternion.rotationAroundZAxis(FastMath.QUARTER_PI)
-		val time = 0.8f // Seconds between frame 1 and 2
-
-		// This is the greatest local force on a tracker, let's assume no angular accel
-		//  for now
-		val centripetalAccel = centripetalAccel(rot1, rot2, length, time)
-
-		// Both our accels should be world space now, so we can add them together
-		// Centripetal accel will be a different magnitude based on tracker placement,
-		//  so let's simulate that
-		val trackerAccel = headAccel + (centripetalAccel * trackerPlacement)
-
-		// Now we can apply a fake attachment (global) and yaw (local)
-		val attachment = Quaternion(1f, 2f, 3f, 4f).unit()
-		val yaw = Quaternion.rotationAroundYAxis(FastMath.HALF_PI)
-
-		// Compute raw versions of our tracker data and ensure the re-calibrated raw
-		//  values are equal to the original ones
-		val rawRot1 = yaw.inv() * rot1 * attachment.inv()
-		assertQuatApproxEqual(rot1, yaw * rawRot1 * attachment)
-		val rawRot2 = yaw.inv() * rot2 * attachment.inv()
-		assertQuatApproxEqual(rot2, yaw * rawRot2 * attachment)
-
-		val rawTrackerAccel = (rot2 * attachment.inv()).inv().sandwich(trackerAccel)
-		assertVectorApproxEqual(trackerAccel, (rot2 * attachment.inv()).sandwich(rawTrackerAccel))
+		val tracker = Tracker(
+			0.2f,
+			0.75f,
+			Quaternion(1f, 2f, 3f, 4f).unit(),
+			Quaternion.rotationAroundYAxis(FastMath.HALF_PI),
+		)
+		val timeline = transformTimeline(
+			tracker,
+			arrayOf(
+				TrackerFrame(Vector3.NULL, Quaternion.IDENTITY, Vector3.NULL, 0f),
+				TrackerFrame(
+					Vector3(0.2f, 0f, 0f),
+					Quaternion.rotationAroundZAxis(FastMath.QUARTER_PI),
+					Vector3.NULL,
+					0.8f,
+				),
+				TrackerFrame(
+					Vector3(-0.2f, 0f, 0.3f),
+					Quaternion.rotationAroundZAxis(FastMath.HALF_PI),
+					Vector3.NULL,
+					0.7f,
+				),
+				TrackerFrame(
+					Vector3(0.0f, 0f, -0.3f),
+					EulerAngles(EulerOrder.YZX, FastMath.QUARTER_PI, FastMath.QUARTER_PI, 0f).toQuaternion(),
+					Vector3.NULL,
+					1.0f,
+				),
+			),
+			::estimateAccel,
+		)
+		val rawTimeline = transformTimeline(
+			tracker,
+			timeline,
+			::calibToRaw,
+		)
 
 		// Error should be about 0 if we have it right
-		val knownError = accelError(headAccel, rawTrackerAccel, rawRot1, rawRot2, length, time, attachment, yaw)
-		assertEquals(0f, knownError, FastMath.ZERO_TOLERANCE, null)
-
-		// Now we have raw and calibrated rotations and local/global accel, let's derive
-		//  our original attachment and yaw given the known head accel and raw tracker
-		//  data :3
+		val knownError = sumAccelError(tracker, rawTimeline)
+		assertEquals(0f, knownError, FastMath.ZERO_TOLERANCE)
 
 		// Let's start by assuming we don't have mounting, but have yaw
-		var newAttachment = attachment.reject(Vector3.POS_Y).unit()
+		val noMountingTracker = tracker.copy(
+			attachmentFix = tracker.attachmentFix.reject(Vector3.POS_Y).unit(),
+		)
 
 		// Now that we don't know attachment, the error should be higher
-		val error = accelError(headAccel, rawTrackerAccel, rawRot1, rawRot2, length, time, newAttachment, yaw)
-		assertNotEquals(0f, error, FastMath.ZERO_TOLERANCE, null)
+		val error = sumAccelError(noMountingTracker, rawTimeline)
+		assertNotEquals(0f, error, FastMath.ZERO_TOLERANCE)
+
+		// TODO Insert optimization algorithm here
+
+		val optimizedTracker = tracker.copy(
+			// trackerPlacement = 0.75f,
+			// attachmentFix = Quaternion.IDENTITY,
+			// yawFix = Quaternion.IDENTITY,
+		)
+		val optimizeError = sumAccelError(optimizedTracker, rawTimeline)
+		assertEquals(0f, optimizeError, FastMath.ZERO_TOLERANCE)
+
+		assertEquals(tracker.trackerPlacement, optimizedTracker.trackerPlacement, FastMath.ZERO_TOLERANCE)
+		assertQuatApproxEqual(tracker.attachmentFix, optimizedTracker.attachmentFix)
+		assertQuatApproxEqual(tracker.yawFix, optimizedTracker.yawFix)
 	}
 }
