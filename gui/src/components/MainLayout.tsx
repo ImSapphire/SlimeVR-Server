f7b1591c4d6af23253239abda46aaa2bb9a97073
Sapphire <imsapphire0@gmail.com>
2026-01-22 13:17:28 -0600

    put reset button and cancel button in grid

--- gui/src/components/MainLayout.tsx
+++ gui/src/components/MainLayout.tsx
@@ -124,12 +124,12 @@ function StepMountingStatusModal({
         <TipBox>{l10n.getString('step_mounting-head_rotation_tip')}</TipBox>
 
         {status >= StepMountingStatus.ERROR_TIMEOUT && (
-          <>
+          <div className="grid grid-cols-2 gap-2">
             <ResetButton type={ResetType.Mounting} group="default" />
             <Button variant="tertiary" onClick={onCancel}>
               <Typography id="step_mounting-cancel" />
             </Button>
-          </>
+          </div>
         )}
       </div>
     </div>
